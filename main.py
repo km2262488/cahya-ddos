20cdd72727b8e6c10567dc135bbb3aa366c39887
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-09 13:15:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def da_delegator(SITE,DOS_TYPE):
     threads = []
     for num in range(thread_count):
         thr1=MyThread(SITE,DOS_TYPE)
-        print("\033[33m[\033[92m+[\033[33m]--- Initiating Attack ---033[0m")
+        print("\033[97m[\033[92m+\033[97m]--- Initiating Attack ---033[0m")
         thr1.start()
         threads.append(thr1)
         #thr1.join()
