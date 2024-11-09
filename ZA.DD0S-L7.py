492427b30a37c241a2af4d866835d12c3e897507
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-09 19:16:50 +0700

    Update ZA.DD0S-L7.py

--- ZA.DD0S-L7.py
+++ ZA.DD0S-L7.py
@@ -130,7 +130,7 @@ def main(argv):
     try:
         opts, args = getopt.getopt(sys.argv[1:], "t:h", ["help","type"])
     except getop.GetoptError (err):
-        print ('str(err')
+        print ('str'(err))
         sys.exit(2)
     output = None
     verbose = False
