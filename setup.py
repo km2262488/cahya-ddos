577b1ad7cc380d3aa0d7f6bad027a70a03375174
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-09 08:12:19 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+     os.system("pip install requests")
+     os.system("pip install setuptool")
+     os.system("pip install wheel")
+ elif c == "1":
+     os.system("pip install requests")
+     os.system("pip install setuptool")
+     os.system("pip install wheel")
+     print("Done.")
