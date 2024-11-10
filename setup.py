645d00b747043923c8ead3be36c743fc55572ea3
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-10 22:44:47 +0700

    Update setup.py
    Signed-off-by: Kafulkaff99 <kafulkaff02@gmail.com>

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ print("""[0] pip\n[1] pip3\nWhich one do you use?""")
 
 c = input(">>>: ")
 if c == "0":
-     os.system("pip install requests")
+     os.system("apt-get install docker")
+     os.system("pip install torch")
      os.system("pip install wheel")
-elif c == "1":
-     os.system("pip3 install requests")
+     os.system("pip install flake8 pytest")
+ elif c == "1":
+     os.system("apt-get install docker")
+     os.system("pip3 install torch")
      os.system("pip3 install wheel")
+     os.system("pip3 install flake8 pytest")
      print("Done.")
+      
